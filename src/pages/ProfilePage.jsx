32237380fa277fd247a881ef0e85348366314080
Yasin Mangera <e1509789@u.nus.edu>
2025-02-21 18:08:37 +0800

    Some more work on validation

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -37,13 +37,42 @@ const ProfilePage = () => {
   const [loading, setLoading] = useState(true);
   const [tabIndex, setTabIndex] = useState(0);
 
+  
+  const validate = () => {
+    const newErrors = {};
+
+    if (!formData.fullName)
+      newErrors.fullName = "Please enter your name!";
+
+    // if (!formInputs.email) newErrors.email = "Please enter email address!";
+    // else if (!/\S+@\S+\.\S+/.test(formInputs.email))
+    //   newErrors.email = "Invalid email format";
+
+    // if (!formData.password) {
+    //   newErrors.password = "Password is required";
+    // } else if (formInputs.password.length < 6) {
+    //   newErrors.password = "Password must be at least 6 characters";
+    // }
+
+    // if (!formData.confirmPassword) {
+    //   newErrors.confirmPassword = "Confirm Password is required";
+    // } else if (formInputs.password !== formInputs.confirmPassword) {
+    //   newErrors.confirmPassword = "Passwords do not match";
+    // }
+
+    setErrors(newErrors);
+
+    // Return first key in error object
+    return Object.keys(newErrors)[0];
+  };
+
   const createUpdateProfile = async () => {
     setLoading(true);
 
     const firstErrorField = validate();
 
     if (!firstErrorField) {
-    if (id) {
+      if (id) {
         await updateProfileDB();
       } else {
         await createProfileDB();
@@ -57,14 +86,10 @@ const ProfilePage = () => {
         });
       }
     }
-    }
     setLoading(false);
   };
 
   const createProfileDB = async () => {
-    setLoading(false);
-  };
-
       const { data, error } = await createProfile({ ...formData }, dispatch);
       if (error) {
         console.error("Error creating profile:", error);
@@ -212,9 +237,12 @@ const ProfilePage = () => {
     dispatch({ type: "LOADING", payload: { isOpen: false } });
   };
 
-  if (loading) return <Typography>Loading...</Typography>;
-  if (error) return <Typography color="error">{error}</Typography>;
-
+  {loading ? (
+    <CircularProgress size={20} className="!text-white" />
+  ) : (
+    "Sign up"
+  )}
+  
   return (
     <Stack className="bg-whiteflex flex-1 items-start justify-start min-h-[100vh] w-full">
       <Navbar />
@@ -306,6 +334,9 @@ const ProfilePage = () => {
                 fullWidth
                 label="Full Name"
                 name="fullName"
+                error={!!errors.fullName}
+            helperText={errors.fullName}
+            inputRef={fieldRefs.fullName}
                 value={formData.fullName}
                 onChange={handleChange}
               />
@@ -314,6 +345,7 @@ const ProfilePage = () => {
                 fullWidth
                 label="Job Title"
                 name="jobTitle"
+            inputRef={fieldRefs.jobTitle}
                 value={formData.jobTitle}
                 onChange={handleChange}
               />
@@ -322,6 +354,7 @@ const ProfilePage = () => {
                 fullWidth
                 label="Current Company"
                 name="currentCompany"
+            inputRef={fieldRefs.currentCompany}
                 value={formData.currentCompany}
                 onChange={handleChange}
               />
@@ -330,6 +363,7 @@ const ProfilePage = () => {
                 fullWidth
                 label="Location"
                 name="location"
+            inputRef={fieldRefs.location}
                 value={formData.location}
                 onChange={handleChange}
               />
@@ -338,6 +372,7 @@ const ProfilePage = () => {
                 fullWidth
                 label="Email"
                 name="email"
+            inputRef={fieldRefs.email}
                 value={formData.email}
                 onChange={handleChange}
               />
@@ -346,6 +381,7 @@ const ProfilePage = () => {
                 fullWidth
                 label="Phone Number"
                 name="phone"
+            inputRef={fieldRefs.phone}
                 value={formData.phone}
                 onChange={handleChange}
               />
@@ -356,6 +392,7 @@ const ProfilePage = () => {
                 name="aboutMe"
                 multiline
                 rows={3}
+            inputRef={fieldRefs.aboutMe}
                 value={formData.aboutMe}
                 onChange={handleChange}
               />
@@ -364,6 +401,7 @@ const ProfilePage = () => {
                 fullWidth
                 label="Programming Languages"
                 name="programmingLanguages"
+            inputRef={fieldRefs.programmingLanguages}
                 value={formData.programmingLanguages}
                 onChange={handleChange}
               />
@@ -372,6 +410,7 @@ const ProfilePage = () => {
                 fullWidth
                 label="Education"
                 name="education"
+            inputRef={fieldRefs.education}
                 value={formData.education}
                 onChange={handleChange}
               />
@@ -380,6 +419,7 @@ const ProfilePage = () => {
                 fullWidth
                 label="Work Experience"
                 name="experience"
+            inputRef={fieldRefs.experience}
                 value={formData.experience}
                 onChange={handleChange}
               />
