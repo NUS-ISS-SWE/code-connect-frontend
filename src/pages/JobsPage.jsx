82621c6f7679ca065a0c640809ff2cca30c28950
jonathantayyw <jonathantayyw@gmail.com>
2025-03-05 23:22:30 +0800

    setup job saerch page [WIP]

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,21 +1,48 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/jsx-no-undef */
+import {
+  Box,
+  Button,
+  InputAdornment,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
-import images from "../assets/images";
+import Icon from "../constants/Icon.jsx";
 import { useGlobalContext } from "../hooks/useGlobalContext";
-import paths from "../routes/paths";
 
-const HomePage = () => {
+const JobsPage = () => {
   const { state, dispatch } = useGlobalContext();
+  const { loading } = state;
+
+  const handleChangeSearchInput = () => {};
 
   return (
     <Stack className="bg-gray-100 flex flex-1 items-start justify-start min-h-[100vh] w-full">
       <Navbar />
 
       <Stack className="flex flex-1 items-start justify-start mx-auto max-w-7xl w-full">
-        Jobs
+        <Stack className="flex justify-start space-y-5 w-full">
+          <TextField
+            className="bg-white w-full"
+            color="primary"
+            disabled={loading.isOpen}
+            fullWidth
+            onChange={handleChangeSearchInput}
+            placeholder={"Search..."}
+            size="small"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment className="!ml-0 !text-gray-400" position="end">
+                  <Icon name={"Search"} size={"1.3em"} />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Stack>
       </Stack>
 
       <Footer />
@@ -23,4 +50,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default JobsPage;
