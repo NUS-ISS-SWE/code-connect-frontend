020fad8ae055aca48efd5576fed7e130aec34c0e
Yasin Mangera <e1509789@u.nus.edu>
2025-03-24 22:59:08 +0800

    Created job card component

--- src/pages/JobListingPage.jsx
+++ src/pages/JobListingPage.jsx
@@ -2,27 +2,24 @@ import {
   Box,
   Button,
   CircularProgress,
-  Divider,
   IconButton,
   InputAdornment,
   Stack,
   TextField,
   Typography,
 } from "@mui/material";
-import { intervalToDuration } from "date-fns";
 import { SelectPicker } from "rsuite";
 import { Link, useSearchParams } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { GetAPI } from "../api/GeneralAPI";
+import JobCard from "../components/jobPageComponents/JobCard";
 
-import dummy from "../assets/dummy/index.js";
 import Icon from "../constants/Icon.jsx";
 import styles from "../constants/styles.jsx";
 import useContent from "../hooks/useContent.js";
 import { useGlobalContext } from "../hooks/useGlobalContext.js";
 import useKeyPress from "../hooks/useKeyPress.js";
-import { renderIntervalDuration } from "../utils/stringUtils.js";
 import {
   JOB_TYPES_FILTER_OPTIONS,
   LOCATION_FILTER_OPTIONS,
@@ -340,71 +337,7 @@ const JobListingPage = () => {
         {filteredJobs?.length > 0 ? (
           filteredJobs?.map((item, index) => {
             return (
-              <Stack
-                className="!bg-white !border !border-gray-300 !border-solid py-2 rounded-md space-y-2 w-full"
-                key={index}
-              >
-                <Box className="flex flex-1 items-start justify-start px-2  space-x-3">
-                  <img
-                    alt={item.jobTitle}
-                    src={item?.thumbnail ?? dummy.jobListings[0].thumbnail}
-                    style={{
-                      height: "auto",
-                      width: "48px",
-                    }}
-                  />
-
-                  <Stack>
-                    <Typography
-                      className="!font-regular !text-sm lg:!text-sm text-start !text-gray-700 hover:underline"
-                      component={Link}
-                    >
-                      {item.companyName}
-                    </Typography>
-                    <Typography
-                      className="!font-regular !text-lg lg:!text-xl text-start !text-primary hover:underline"
-                      component={Link}
-                      to={paths.get("GETJOB").PATH.replace(":id", item.id)}
-                    >
-                      {item.jobTitle}
-                    </Typography>
-                  </Stack>
-                </Box>
-
-                <Box className="flex flex-1 items-center justify-start px-2  space-x-1 !text-gray-700">
-                  <Typography className="!font-regular !text-sm lg:!text-xs text-start !text-gray-500">
-                    {item.jobType}
-                  </Typography>
-
-                  <Icon name={"Dot"} size={"1em"} />
-
-                  <Typography className="!font-regular !text-sm lg:!text-xs text-start !text-gray-500">
-                    {item.location}
-                  </Typography>
-
-                  <Typography className="!font-regular !text-sm lg:!text-xs text-start !text-gray-500">
-                    {`Posted ${item.postedDate ? new Date(item.postedDate).toLocaleDateString() : ""}`}
-                    {/* {`Posted ${renderIntervalDuration(
-                    item.postedDate,
-                    intervalToDuration
-                  )}`} */}
-                  </Typography>
-                </Box>
-
-                <Divider flexItem />
-
-                <Box className="flex flex-1 items-start justify-start px-2  space-x-1">
-                  <Typography className="!font-regular !text-sm lg:!text-xs text-start !text-gray-500">
-                    {`${item.numberApplied} applied`}
-                  </Typography>
-
-                  <Icon name={"Dot"} size={"1em"} />
-
-                  <Typography className="!font-regular !text-sm lg:!text-xs text-start !text-primary">
-                    {item.salaryRange}
-                  </Typography>
-                </Box>
-              </Stack>
+              <JobCard item={item} index={index}/>
             );
           })
         ) : (
